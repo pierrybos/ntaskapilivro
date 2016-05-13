66849916731a2150ba58a53c792bc5b23f10277f
bos, pierry <bos@pierry.net>
2016-05-13 07:57:04 -0300

    final

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -3,6 +3,7 @@ var express = require('express');
 var morgan = require('morgan');
 var cors = require('cors');
 var compression = require('compression');
+var helmet = require('helmet');
 var logger = require('./logger.js');
 
 module.exports = function(app){
@@ -15,10 +16,11 @@ module.exports = function(app){
     		}
     	}
     }));
+    app.use(helmet());
     app.use(cors({
-    	origin: ["http://localhost:3001", "http://localhost:3000", "https://localhost:3001", "https://localhost:3000"],
-    	methods:["GET", "POST", "PUT", "DELETE"],
-    	allowedHeaders: ["Content-Type", "Authorization"]
+        origin: ["http://localhost:3001", "http://localhost:3000", "https://localhost:3001", "https://localhost:3000"],
+        methods:["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
     }));
     app.use(compression());
     app.use(bodyParser.json());
--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -6,7 +6,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-05-11T11:27:38.971Z",
+    "time": "2016-05-13T00:51:14.697Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
